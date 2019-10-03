1cfc4c2f804219b01e4a329b2016fca2968c54b7
Robert Wise <robcodehub@gmail.com>
2019-10-03 09:59:28 +0700

    set up express server and started on db

--- db.js
+++ db.js
@@ -1,5 +1,11 @@
+const pg = require('pg');
 
+const Sequelize = require('sequelize');
+const { UUID, UUIDV4, STRING } = Sequelize;
 
 //SQL DATABASE acme-schools
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme-schools');
+
+
 
 
--- server.js
+++ server.js
@@ -1,7 +1,15 @@
+const express = require('express');
+const app = express();
+const db = require('./db');
 
+const port = process.env.PORT || 3010;
 
+//CHANGE TO db.syncAndSeed & .then
+app.listen(port, ()=> console.log(`listening on port ${port}`));
 
 // Route /api/students
 
 
-// Route  /api/schools 
+// Route  /api/schools
+
+
