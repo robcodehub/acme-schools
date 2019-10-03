f7316dff10b0b5de01fab217cb9151e29d6b229d
Robert Wise <robcodehub@gmail.com>
2019-10-03 11:13:57 +0700

    working on api/students route

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const db = require('./db');
+const { models } = require('./db');
+const { Student, School } = models;
 
 const path = require('path');
 
@@ -10,10 +12,14 @@ const port = process.env.PORT || 3010;
 app.listen(port, ()=> console.log(`listening on port ${port}`));
 
 //Home page
-app.get('/', (req, req, next) => res.sendFile(path.join(__dirname, "index.html")));
+app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, "index.html")));
 
 // Route /api/students
-
+app.get('/api/students', (req, res, next) => {
+  Student.findAll()
+  .then(students => res.send(students))
+  .catch(next)
+});
 
 // Route  /api/schools
 
