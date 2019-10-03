cab3cd9bc45a0455483838bdb9ee76c4edefad64
Robert Wise <robcodehub@gmail.com>
2019-10-03 11:03:24 +0700

    express server renders index.html home page

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ const express = require('express');
 const app = express();
 const db = require('./db');
 
+const path = require('path');
+
 const port = process.env.PORT || 3010;
 
 //CHANGE TO db.syncAndSeed & .then
 app.listen(port, ()=> console.log(`listening on port ${port}`));
+
+//Home page
+app.get('/', (req, req, next) => res.sendFile(path.join(__dirname, "index.html")));
 
 // Route /api/students
 
