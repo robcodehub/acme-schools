2859ffb768cd484dfd4eaeb419fde003414c7d3c
Robert Wise <robcodehub@gmail.com>
2019-10-03 10:06:25 +0700

    created school in db - checking imageURL datatype in sequelize

--- db.js
+++ db.js
@@ -6,6 +6,15 @@ const { UUID, UUIDV4, STRING } = Sequelize;
 //SQL DATABASE acme-schools
 const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme-schools');
 
+const uuidStandard = {
+  type: UUID,
+  primaryKey: true,
+  defaultValue: UUIDV4
+};
 
-
+const School = conn.define('school', {
+  id: uuidStandard,
+  name: STRING,
+  imageURL: STRING
+})
 
