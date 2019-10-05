1598148eb0174075669f7f4a84b0e7db30cf0a3e
Robert Wise <robcodehub@gmail.com>
2019-10-06 00:02:17 +0700

    fixed error with postgres not running

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const path = require('path');
 const port = process.env.PORT || 3010;
 
 
-
 //Home page
 app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, "index.html")));
 
@@ -20,26 +19,37 @@ app.get('/api/students', (req, res, next) => {
   .catch(next)
 });
 
+// Route  /api/schools
+app.get('/api/schools', (req, res, next) => {
+  School.findAll()
+  .then(schools => res.send(schools))
+  .catch(next)
+});
+
+
 //STEP 1 - FINISH ROUTES
 
 //CREATE A STUDENT ROUTE - POST ROUTE
 //RETURN CREATED STUDENT WITH REDUX
 //SEND INFO BACK TO FRONT END TO UPDATE STATE
+/*
 app.post('/api/students', (req, res, next) => {
   Student.create(req.body)
   .then(newStudent => res.status(201).send(newStudent))
   .catch(next)
 });
-
+*/
 //DELETE A STUDENT - DELETE ROUTE
 //DELETED STUDENT NOT RETURNED
 //CHECK IF SUCCESSFUL OR NOT AND THEN DELETE STUDENT ON FRONT END
+/*
 app.delete('/api/students/:id', (req, res, next) => {
   Student.findByPk(req.params.id)
   .then(currentStudent => currentStudent.destroy())
   .then(() => res.sendStatus(204))
   .catch(next)
 });
+*/
 
 //UPDATE A STUDENT - PUT ROUTE OR PATCH ROUTE
 //PATCH ROUTE FOR A SPECIFIC FIELD - UPDATE SCHOOL FOR STUDENT
@@ -48,13 +58,6 @@ app.delete('/api/students/:id', (req, res, next) => {
 //REMOVE STUDENT FROM SCHOOL - PUT ROUTE OR PATCH ROUTE
 //PATCH ROUTE FOR A SPECIFIC FIELD - UPDATE SCHOOL FOR STUDENT
 
-
-// Route  /api/schools
-app.get('/api/schools', (req, res, next) => {
-  School.findAll()
-  .then(schools => res.send(schools))
-  .catch(next)
-});
 
 db.syncAndSeed()
 .then(() => {
